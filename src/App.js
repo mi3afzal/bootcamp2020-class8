c415a0ba017f75e8a02596f861de77e700b3e528
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2020-06-29 15:45:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
 			<ol>
 				{repos.map((repoObj, index) => {
 					return (<li key={index}>
-						<span className="repo-name"><a href={repoObj.html_url} target="blank">{repoObj.name}</a></span> 
+						<span className="repo-name"> <a href={repoObj.html_url} target="blank">{repoObj.name}</a> </span> 
 						{ repoObj.description && 
-							<span className="repo-desc">(${repoObj.description})</span>
+							<span className="repo-desc"> (${repoObj.description}) </span>
 						}
 					</li>)
 				})}
